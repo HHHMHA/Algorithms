193d23e851588b5798282f98d82a7a253a259061
J2mF <mp4-12cs5@hotmail.com>
2021-12-25 11:25:13 +0200

    [WIP] Implement LinkedList
    Change Log:
    - Implement Node class

---
+++ src/main/java/collections/List.java
@@ -0,0 +1,8 @@
+package collections;
+
+public interface List {
+    void insert( int element, int index );
+    void delete( int index );
+    int find( int element );
+    int get( int index );
+}
---
+++ src/main/java/collections/Node.java
@@ -0,0 +1,37 @@
+package collections;
+
+public class Node {
+    public static final Node nill = new Node();
+
+    private Integer value;
+    private Node next;
+    private Node previous;
+
+    private Node() {
+        next = null;
+        previous = null;
+        value = null;
+    }
+
+    public Node( int value, Node next, Node previous ) {
+        this.value = value;
+        this.next = next;
+        this.previous = previous;
+    }
+
+    public Node( int value ) {
+        this( value, nill, nill );
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public Node getPrevious() {
+        return previous;
+    }
+}

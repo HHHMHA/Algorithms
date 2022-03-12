8e1df1803921c91a00b02623a01e95e01263b1f8
J2mF <mp4-12cs5@hotmail.com>
2022-03-12 12:41:34 +0200

    Implement ListQueue

---
+++ src/main/java/collections/ListQueue.java
@@ -0,0 +1,38 @@
+package collections;
+
+public class ListQueue implements Queue {
+    private final List list = new LinkedList();
+
+    @Override
+    public Integer peek() {
+        if ( isEmpty() )
+            return null;
+        return list.get( 0 );
+    }
+
+    @Override
+    public void enqueue( Integer element ) {
+        list.insert( element, getSize() );
+    }
+
+    // TODO: this method will always have this implementation so why override it?
+    @Override
+    public boolean isEmpty() {
+        return getSize() == 0;
+    }
+
+    @Override
+    public int getSize() {
+        return list.getSize();
+    }
+
+    @Override
+    public Integer dequeue() {
+        if ( isEmpty() )
+            return null;
+
+        int element = peek();
+        list.delete( 0 );
+        return element;
+    }
+}

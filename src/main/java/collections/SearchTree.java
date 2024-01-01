af266617d6c6a62bf4590181d800fdc526fa192c
J2mF <mp4-12cs5@hotmail.com>
2024-01-01 21:17:33 +0300

    feat(BST): Implement Delete for BST (Closes #2)

--- src/main/java/collections/SearchTree.java
+++ src/main/java/collections/SearchTree.java
@@ -5,61 +5,61 @@ public class SearchTree implements Tree {
     private TreeNode root = TreeNode.nill;
 
     @Override
-    public TreeNode find( int element ) {
+    public TreeNode find(int element) {
         var node = root;
-        while ( node != TreeNode.nill ) {
-            if ( node.getValue() == element ) {
+        while (node != TreeNode.nill) {
+            if (node.getValue() == element) {
                 return node;
             }
 
-            node = moveTowards( element, node );
+            node = moveTowards(element, node);
         }
         return node;
     }
 
     // TODO: move to TreeNode
-    private TreeNode moveTowards( int element, TreeNode node ) {
+    private TreeNode moveTowards(int element, TreeNode node) {
         return node.getValue() < element ? node.getRight() : node.getLeft();
     }
 
     @Override
     public TreeNode max() {
-        return max( root );
+        return max(root);
     }
 
     @Override
-    public TreeNode max( TreeNode node ) {
-        if ( node == TreeNode.nill )
+    public TreeNode max(TreeNode node) {
+        if (node == TreeNode.nill)
             return node;
 
-        while ( node.getRight() != TreeNode.nill )
+        while (node.getRight() != TreeNode.nill)
             node = node.getRight();
         return node;
     }
 
     @Override
     public TreeNode min() {
-        return min( root );
+        return min(root);
     }
 
     @Override
-    public TreeNode min( TreeNode node ) {
-        if ( node == TreeNode.nill )
+    public TreeNode min(TreeNode node) {
+        if (node == TreeNode.nill)
             return node;
 
-        while ( node.getLeft() != TreeNode.nill )
+        while (node.getLeft() != TreeNode.nill)
             node = node.getLeft();
         return node;
     }
 
     @Override
-    public TreeNode successor( TreeNode node ) {
+    public TreeNode successor(TreeNode node) {
         if (node == TreeNode.nill) {
             return TreeNode.nill;
         }
 
-        if ( node.getRight() != TreeNode.nill ) {
-            return min( node.getRight() );
+        if (node.getRight() != TreeNode.nill) {
+            return min(node.getRight());
         }
 
         var parent = node.getParent();
@@ -72,13 +72,13 @@ public class SearchTree implements Tree {
     }
 
     @Override
-    public TreeNode predecessor( TreeNode node ) {
+    public TreeNode predecessor(TreeNode node) {
         if (node == TreeNode.nill) {
             return TreeNode.nill;
         }
 
-        if ( node.getLeft() != TreeNode.nill ) {
-            return min( node.getLeft() );
+        if (node.getLeft() != TreeNode.nill) {
+            return min(node.getLeft());
         }
 
         var parent = node.getParent();
@@ -91,52 +91,108 @@ public class SearchTree implements Tree {
     }
 
     @Override
-    public void insert( int element ) {
-        if ( isDuplicate( element ) ) {
+    public void insert(int element) {
+        if (isDuplicate(element)) {
             return;
         }
 
-        var node = new TreeNode( element );
+        var node = new TreeNode(element);
 
-        if ( isEmpty() ) {
+        if (isEmpty()) {
             root = node;
             ++size;
             return;
         }
 
         ++size;
-        TreeNode parent = getNewParent( element );
+        TreeNode parent = getNewParent(element);
 
-        setParent( node, parent );
+        setParent(node, parent);
     }
 
     // TODO: move to TreeNode
-    private void setParent( TreeNode node, TreeNode parent ) {
-        node.setParent( parent );
-        if ( parent.getValue() < node.getValue() ) {
-            parent.setRight( node );
+    private void setParent(TreeNode node, TreeNode parent) {
+        node.setParent(parent);
+        if (parent.getValue() < node.getValue()) {
+            parent.setRight(node);
         } else {
-            parent.setLeft( node );
+            parent.setLeft(node);
         }
     }
 
-    private TreeNode getNewParent( int element ) {
+    private TreeNode getNewParent(int element) {
         var parent = TreeNode.nill;
         var target = root;
-        while ( target != TreeNode.nill ) {
+        while (target != TreeNode.nill) {
             parent = target;
-            target = moveTowards( element, target );
+            target = moveTowards(element, target);
         }
         return parent;
     }
 
-    private boolean isDuplicate( int element ) {
-        return find( element ) != TreeNode.nill;
+    private boolean isDuplicate(int element) {
+        return find(element) != TreeNode.nill;
     }
 
     @Override
-    public TreeNode delete( int element ) {
-        return null;
+    public TreeNode delete(int element) {
+        if (isEmpty()) {
+            return TreeNode.nill;
+        }
+
+        TreeNode nodeToDelete = find(element);
+        if (nodeToDelete == TreeNode.nill) {
+            return TreeNode.nill;
+        }
+
+        TreeNode parent = nodeToDelete.getParent();
+
+        // Case 1: Node to delete is a leaf node
+        if (nodeToDelete.getLeft() == TreeNode.nill && nodeToDelete.getRight() == TreeNode.nill) {
+            if (parent == TreeNode.nill) {
+                root = TreeNode.nill;
+            } else if (parent.getLeft() == nodeToDelete) {
+                parent.setLeft(TreeNode.nill);
+            } else {
+                parent.setRight(TreeNode.nill);
+            }
+        }
+        // Case 2: Node to delete has only one child
+        else if (nodeToDelete.getLeft() == TreeNode.nill || nodeToDelete.getRight() == TreeNode.nill) {
+            TreeNode child = (nodeToDelete.getLeft() != TreeNode.nill) ? nodeToDelete.getLeft() : nodeToDelete.getRight();
+
+            if (parent == TreeNode.nill) {
+                root = child;
+            } else if (parent.getLeft() == nodeToDelete) {
+                parent.setLeft(child);
+            } else {
+                parent.setRight(child);
+            }
+            child.setParent(parent);
+        }
+        // Case 3: Node to delete has two children
+        else {
+            TreeNode successor = successor(nodeToDelete);
+
+            if (successor.getRight() != TreeNode.nill) {
+                successor.getRight().setParent(successor.getParent());
+            }
+            successor.setLeft(nodeToDelete.getLeft());
+            successor.setRight(nodeToDelete.getRight());
+            successor.setParent(parent);
+            if (parent == TreeNode.nill) {
+                root = successor;
+            }
+            if (parent.getRight() == nodeToDelete) {
+                parent.setRight(successor);
+            }
+            if (parent.getLeft() == nodeToDelete) {
+                parent.setLeft(successor);
+            }
+        }
+
+        size--;
+        return nodeToDelete;
     }
 
     @Override
--- src/test/java/collections/SearchTreeTest.java
+++ src/test/java/collections/SearchTreeTest.java
@@ -9,242 +9,328 @@ class SearchTreeTest {
     @Test
     void emptyTree() {
         Tree t = new SearchTree();
-        assertTrue( t.isEmpty() );
-        assertEquals( 0, t.getSize() );
+        assertTrue(t.isEmpty());
+        assertEquals(0, t.getSize());
     }
 
     @Test
     void findWhenTreeEmpty() {
         Tree t = new SearchTree();
-        assertEquals( TreeNode.nill, t.find( 0 ) );
+        assertEquals(TreeNode.nill, t.find(0));
     }
 
     @Test
     void find() {
         Tree t = new SearchTree();
-        t.insert( 1 );
-        t.insert( 2 );
-        t.insert( 0 );
-        assertEquals( 1, t.find( 1 ).getValue() );
-        assertEquals( 2, t.find( 2 ).getValue() );
-        assertEquals( 0, t.find( 0 ).getValue() );
+        t.insert(1);
+        t.insert(2);
+        t.insert(0);
+        assertEquals(1, t.find(1).getValue());
+        assertEquals(2, t.find(2).getValue());
+        assertEquals(0, t.find(0).getValue());
     }
 
     @Test
     void max() {
         Tree t = new SearchTree();
-        assertEquals( TreeNode.nill, t.max() );
+        assertEquals(TreeNode.nill, t.max());
 
-        t.insert( 1 );
-        assertEquals( 1, t.max().getValue() );
+        t.insert(1);
+        assertEquals(1, t.max().getValue());
 
-        t.insert( 10 );
-        assertEquals( 10, t.max().getValue() );
+        t.insert(10);
+        assertEquals(10, t.max().getValue());
 
-        t.insert( 2 );
-        assertEquals( 10, t.max().getValue() );
+        t.insert(2);
+        assertEquals(10, t.max().getValue());
     }
 
     @Test
     void maxWithParam() {
         Tree t = new SearchTree();
-        assertEquals( TreeNode.nill, t.max( TreeNode.nill ) );
+        assertEquals(TreeNode.nill, t.max(TreeNode.nill));
 
-        t.insert( 5 );
-        t.insert( 10 );
-        t.insert( 3 );
-        t.insert( 4 );
-        t.insert( 1 );
+        t.insert(5);
+        t.insert(10);
+        t.insert(3);
+        t.insert(4);
+        t.insert(1);
 
-        var node = t.find( 3 );
-        assertEquals( 4, t.max( node ).getValue() );
+        var node = t.find(3);
+        assertEquals(4, t.max(node).getValue());
     }
 
     @Test
     void min() {
         Tree t = new SearchTree();
-        assertEquals( TreeNode.nill, t.min() );
+        assertEquals(TreeNode.nill, t.min());
 
-        t.insert( 2 );
-        assertEquals( 2, t.min().getValue() );
+        t.insert(2);
+        assertEquals(2, t.min().getValue());
 
-        t.insert( 10 );
-        assertEquals( 2, t.min().getValue() );
+        t.insert(10);
+        assertEquals(2, t.min().getValue());
 
-        t.insert( 1 );
-        assertEquals( 1, t.min().getValue() );
+        t.insert(1);
+        assertEquals(1, t.min().getValue());
     }
 
     @Test
     void minWithParam() {
         Tree t = new SearchTree();
-        assertEquals( TreeNode.nill, t.min( TreeNode.nill ) );
+        assertEquals(TreeNode.nill, t.min(TreeNode.nill));
 
-        t.insert( 5 );
-        t.insert( 10 );
-        t.insert( 3 );
-        t.insert( 4 );
-        t.insert( 1 );
+        t.insert(5);
+        t.insert(10);
+        t.insert(3);
+        t.insert(4);
+        t.insert(1);
 
-        var node = t.find( 3 );
-        assertEquals( 1, t.min( node ).getValue() );
+        var node = t.find(3);
+        assertEquals(1, t.min(node).getValue());
     }
 
     @Test
     void successorRightNodeFull() {
         Tree t = new SearchTree();
-        assertEquals( TreeNode.nill, t.successor( TreeNode.nill ) );
+        assertEquals(TreeNode.nill, t.successor(TreeNode.nill));
 
-        t.insert( 5 );
-        t.insert( 10 );
-        t.insert( 7 );
-        t.insert( 20 );
-        t.insert( 3 );
-        t.insert( 4 );
-        t.insert( 1 );
-        t.insert( 6 );
+        t.insert(5);
+        t.insert(10);
+        t.insert(7);
+        t.insert(20);
+        t.insert(3);
+        t.insert(4);
+        t.insert(1);
+        t.insert(6);
 
-        var node = t.find( 5 );
-        var successor = t.successor( node );
-        var actualSuccessor = t.find( 6 );
-        assertEquals( actualSuccessor, successor );
+        var node = t.find(5);
+        var successor = t.successor(node);
+        var actualSuccessor = t.find(6);
+        assertEquals(actualSuccessor, successor);
     }
 
     @Test
     void successorNoRightNode() {
         Tree t = new SearchTree();
-        assertEquals( TreeNode.nill, t.successor( TreeNode.nill ) );
+        assertEquals(TreeNode.nill, t.successor(TreeNode.nill));
 
-        t.insert( 5 );
-        t.insert( 10 );
-        t.insert( 7 );
-        t.insert( 20 );
-        t.insert( 3 );
-        t.insert( 4 );
-        t.insert( 1 );
-        t.insert( 6 );
-        t.insert( 8 );
+        t.insert(5);
+        t.insert(10);
+        t.insert(7);
+        t.insert(20);
+        t.insert(3);
+        t.insert(4);
+        t.insert(1);
+        t.insert(6);
+        t.insert(8);
 
-        var node = t.find( 8 );
-        var successor = t.successor( node );
-        var actualSuccessor = t.find( 10 );
-        assertEquals( actualSuccessor, successor );
+        var node = t.find(8);
+        var successor = t.successor(node);
+        var actualSuccessor = t.find(10);
+        assertEquals(actualSuccessor, successor);
     }
 
     @Test
     void successorNoSuccessor() {
         Tree t = new SearchTree();
-        assertEquals( TreeNode.nill, t.successor( TreeNode.nill ) );
+        assertEquals(TreeNode.nill, t.successor(TreeNode.nill));
 
-        t.insert( 5 );
-        t.insert( 10 );
-        t.insert( 7 );
-        t.insert( 3 );
-        t.insert( 4 );
-        t.insert( 1 );
-        t.insert( 6 );
-        t.insert( 8 );
+        t.insert(5);
+        t.insert(10);
+        t.insert(7);
+        t.insert(3);
+        t.insert(4);
+        t.insert(1);
+        t.insert(6);
+        t.insert(8);
 
-        var node = t.find( 10 );
-        var successor = t.successor( node );
+        var node = t.find(10);
+        var successor = t.successor(node);
         var actualSuccessor = TreeNode.nill;
-        assertEquals( actualSuccessor, successor );
+        assertEquals(actualSuccessor, successor);
     }
 
     @Test
     void predecessorLeftNodeFull() {
         Tree t = new SearchTree();
-        assertEquals( TreeNode.nill, t.predecessor( TreeNode.nill ) );
+        assertEquals(TreeNode.nill, t.predecessor(TreeNode.nill));
 
-        t.insert( 5 );
-        t.insert( 10 );
-        t.insert( 7 );
-        t.insert( 20 );
-        t.insert( 3 );
-        t.insert( 4 );
-        t.insert( 1 );
-        t.insert( 6 );
+        t.insert(5);
+        t.insert(10);
+        t.insert(7);
+        t.insert(20);
+        t.insert(3);
+        t.insert(4);
+        t.insert(1);
+        t.insert(6);
 
-        var node = t.find( 5 );
-        var predecessor = t.predecessor( node );
-        var actualPredecessor = t.find( 1 );
-        assertEquals( actualPredecessor, predecessor );
+        var node = t.find(5);
+        var predecessor = t.predecessor(node);
+        var actualPredecessor = t.find(1);
+        assertEquals(actualPredecessor, predecessor);
     }
 
     @Test
     void predecessorNoLeftNode() {
         Tree t = new SearchTree();
-        assertEquals( TreeNode.nill, t.predecessor( TreeNode.nill ) );
+        assertEquals(TreeNode.nill, t.predecessor(TreeNode.nill));
 
-        t.insert( 5 );
-        t.insert( 1 );
-        t.insert( 10 );
-        t.insert( 20 );
-        t.insert( 15 );
+        t.insert(5);
+        t.insert(1);
+        t.insert(10);
+        t.insert(20);
+        t.insert(15);
 
-        var node = t.find( 10 );
-        var predecessor = t.predecessor( node );
-        var actualPredecessor = t.find( 5 );
-        assertEquals( actualPredecessor, predecessor );
+        var node = t.find(10);
+        var predecessor = t.predecessor(node);
+        var actualPredecessor = t.find(5);
+        assertEquals(actualPredecessor, predecessor);
     }
 
     @Test
     void predecessorNoPredecessor() {
         Tree t = new SearchTree();
-        assertEquals( TreeNode.nill, t.predecessor( TreeNode.nill ) );
+        assertEquals(TreeNode.nill, t.predecessor(TreeNode.nill));
 
-        t.insert( 5 );
-        t.insert( 10 );
-        t.insert( 7 );
-        t.insert( 20 );
-        t.insert( 6 );
+        t.insert(5);
+        t.insert(10);
+        t.insert(7);
+        t.insert(20);
+        t.insert(6);
 
-        var node = t.find( 5 );
-        var predecessor = t.predecessor( node );
+        var node = t.find(5);
+        var predecessor = t.predecessor(node);
         var actualPredecessor = TreeNode.nill;
-        assertEquals( actualPredecessor, predecessor );
+        assertEquals(actualPredecessor, predecessor);
     }
 
     @Test
     void insertWhenTreeEmpty() {
         Tree t = new SearchTree();
-        t.insert( 1 );
-        var node = t.find( 1 );
-        assertEquals( 1, node.getValue() );
-        assertEquals( TreeNode.nill, node.getRight() );
-        assertEquals( TreeNode.nill, node.getLeft() );
-        assertEquals( TreeNode.nill, node.getParent() );
-        assertEquals( 1, t.getSize() );
-        assertFalse( t.isEmpty() );
+        t.insert(1);
+        var node = t.find(1);
+        assertEquals(1, node.getValue());
+        assertEquals(TreeNode.nill, node.getRight());
+        assertEquals(TreeNode.nill, node.getLeft());
+        assertEquals(TreeNode.nill, node.getParent());
+        assertEquals(1, t.getSize());
+        assertFalse(t.isEmpty());
     }
 
     @Test
     void insertMultipleElementsThenCorrectRelation() {
         Tree t = new SearchTree();
-        t.insert( 1 );
-        t.insert( 2 );
-        t.insert( 0 );
-        var node = t.find( 1 );
+        t.insert(1);
+        t.insert(2);
+        t.insert(0);
+        var node = t.find(1);
         var left = node.getLeft();
         var right = node.getRight();
 
-        assertEquals( TreeNode.nill, node.getParent() );
-        assertEquals( 3, t.getSize() );
+        assertEquals(TreeNode.nill, node.getParent());
+        assertEquals(3, t.getSize());
 
         // Left node properties
-        assertEquals( 0, left.getValue() );
-        assertEquals( TreeNode.nill, left.getRight() );
-        assertEquals( TreeNode.nill, left.getLeft() );
-        assertEquals( node.getValue(), left.getParent().getValue() );
+        assertEquals(0, left.getValue());
+        assertEquals(TreeNode.nill, left.getRight());
+        assertEquals(TreeNode.nill, left.getLeft());
+        assertEquals(node.getValue(), left.getParent().getValue());
 
         // Right node properties
-        assertEquals( 2, right.getValue() );
-        assertEquals( TreeNode.nill, right.getRight() );
-        assertEquals( TreeNode.nill, right.getLeft() );
-        assertEquals( node.getValue(), right.getParent().getValue() );
+        assertEquals(2, right.getValue());
+        assertEquals(TreeNode.nill, right.getRight());
+        assertEquals(TreeNode.nill, right.getLeft());
+        assertEquals(node.getValue(), right.getParent().getValue());
     }
 
     @Test
-    void delete() {
+    void deleteWhenTreeEmpty() {
+        Tree t = new SearchTree();
+        assertEquals(TreeNode.nill, t.delete(0));
+        assertEquals(TreeNode.nill, t.max());
+        assertEquals(TreeNode.nill, t.min());
+        assertEquals(0, t.getSize());
+        assertTrue(t.isEmpty());
+    }
+
+    @Test
+    void deleteNonExistentElement() {
+        Tree t = new SearchTree();
+        t.insert(1);
+        t.insert(2);
+        t.insert(3);
+
+        assertEquals(TreeNode.nill, t.delete(0));
+        assertEquals(3, t.getSize());
+    }
+
+    @Test
+    void deleteLeafNode() {
+        Tree t = new SearchTree();
+        t.insert(2);
+        t.insert(1);
+        t.insert(3);
+
+        assertEquals(1, t.delete(1).getValue());
+        assertEquals(TreeNode.nill, t.find(1));
+        assertEquals(2, t.getSize());
+    }
+
+    @Test
+    void deleteNodeWithOneChild() {
+        Tree t = new SearchTree();
+        t.insert(2);
+        t.insert(1);
+        t.insert(3);
+        t.insert(4);
+
+        assertEquals(3, t.delete(3).getValue());
+        assertEquals(TreeNode.nill, t.find(3));
+        assertEquals(4, t.find(4).getValue());
+        assertEquals(1, t.find(1).getValue());
+        assertEquals(2, t.find(2).getValue());
+        assertEquals(3, t.getSize());
+    }
+
+    @Test
+    void deleteRootWithTwoChildren() {
+        Tree t = new SearchTree();
+        t.insert(5);
+        t.insert(3);
+        t.insert(7);
+        t.insert(2);
+        t.insert(4);
+        t.insert(6);
+        t.insert(8);
+
+        assertEquals(5, t.delete(5).getValue());
+        assertEquals(TreeNode.nill, t.find(5));
+        assertEquals(6, t.getSize());
+        assertEquals(8, t.max().getValue());
+        assertEquals(2, t.min().getValue());
+    }
+
+    @Test
+    void deleteNodeWithTwoChildren() {
+        Tree t = new SearchTree();
+        t.insert(5);
+        t.insert(3);
+        t.insert(7);
+        t.insert(2);
+        t.insert(4);
+        t.insert(6);
+        t.insert(8);
+
+        assertEquals(3, t.delete(3).getValue());
+        assertEquals(TreeNode.nill, t.find(3));
+        assertEquals(6, t.getSize());
+
+        assertEquals(5, t.find(5).getValue());
+        assertEquals(8, t.find(8).getValue());
+        assertEquals(7, t.find(7).getValue());
+        assertEquals(2, t.find(2).getValue());
+        assertEquals(4, t.find(4).getValue());
+        assertEquals(6, t.find(6).getValue());
     }
 }
